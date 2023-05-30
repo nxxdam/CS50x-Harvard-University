e06b98aea941c6139a078a1e650f1097a2e63540
Anis Damirah <86903348+nxxdam@users.noreply.github.com>
2023-05-30 14:46:53 +0800

    Create 11.sql

---
+++ pset7/movies/11.sql
@@ -0,0 +1,10 @@
+
+
+SELECT movies.title
+FROM stars
+JOIN movies ON stars.movie_id = movies.id
+JOIN people ON stars.person_id = people.id
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC
+LIMIT 5;
